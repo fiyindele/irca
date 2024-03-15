e8962ed86a8c73abe0578151c8921828640650a8
fiyin Oladele <fiyin_oladele@yahoo.com>
2024-03-15 12:01:51 -0700

    working with the activity API

--- irca-react/src/App.jsx
+++ irca-react/src/App.jsx
@@ -2,14 +2,14 @@ import "./App.css";
 import Heading from "./Heading.jsx";
 import List from "./List.jsx";
 import CountButton from "./CountButton.jsx";
+import Activity from "./components/Activity";
 
 function App() {
   return (
     <>
       <div></div>
       <Heading />
-      <CountButton />
-      <List />
+      <Activity />
     </>
   );
 }
--- irca-react/src/components/Activity.jsx
+++ irca-react/src/components/Activity.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "../App.css";
 import axios from "axios";
 
@@ -6,26 +6,53 @@ const Activity = () => {
   const [count, setCount] = useState(0);
   const [data, setData] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          "https://bored-api.appbrewery.com/random"
-        );
-        setData(response.data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+  const url = "https://bored-api.appbrewery.com/random";
 
+  // fetch(url)
+  //   .then((response) => response.json())
+  //   .then((json) => displayResults(json))
+  //   .catch((error) => console.error(`Error fetching dataa: ${error}`));
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(url);
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
+  const handleButtonClick = () => {
+    setCount((prevCount) => prevCount + 1);
     fetchData();
-  }, []);
+  };
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         "https://bored-api.appbrewery.com/random"
+  //       );
+  //       setData(response.data);
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+  // function displayResults(params) {}
 
   return (
     <div className="card">
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <button onClick={handleButtonClick()}>count is {count}</button>
+      {data && (
+        <div>
+          <h2>Activity: {data.activity}</h2>
+          <h3>Type: {data.type}</h3>
+        </div>
+      )}
     </div>
   );
 };
